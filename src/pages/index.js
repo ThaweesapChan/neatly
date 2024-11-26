af31d4a31b47e1cbe41b5fecfefcdb089c4633ca
thaweesap chantasathanont <thaweesap@192.168.1.55>
2024-11-26 10:01:43 +0700

    feat: add button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="">test</h1>
-      <button className="btn-pri-org600">button</button>
+      <h1 className="">test</h1>      
+      <di></di>
     </div>
   );
 }
