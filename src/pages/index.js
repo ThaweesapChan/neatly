c6635288276b25f3f01e45726efd838e152c6e6f
Kong Thiraphat <thiraphat.cto@gmail.com>
2024-11-26 10:08:41 +0700

    fix: style button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      <h1 className="">test</h1>
-      <button className="btn-pri-org600">button</button>
-    </div>
-  );
+  return <div></div>;
 }
