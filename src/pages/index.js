66e6af590f4ac59e99796a675b7cc1e795755d77
Kong Thiraphat <thiraphat.cto@gmail.com>
2024-11-27 22:29:38 +0700

    fix: text color in footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,5 @@ import Homepage from "./homepage";
 import Navbar from "@/component/navbar";
 import Footer from "@/component/footer";
 export default function Home() {
-
-  return (
-    <div>
- 
-    </div>
-  );
+  return <div></div>;
 }
