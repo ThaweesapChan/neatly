0f9ec617a16aaa753ecb02d1005031a0ac0c3716
Arisa Teandum <basarisa.1302@gmail.com>
2024-11-27 20:33:43 +0700

    fix: width in herosection

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
 import Homepage from "./homepage";
-import Navbar from "@/component/navbar";
-import Footer from "@/component/footer";
 export default function Home() {
 
   return (
     <div>
- 
+      <Homepage/>
     </div>
   );
 }
