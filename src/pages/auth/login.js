c615741421c490ed898c42de343b509ed470e9b6
Arisa Teandum <basarisa.1302@gmail.com>
2024-11-28 23:22:33 +0700

    feat: add validating user input for username and email

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -1,12 +1,51 @@
 import { useState } from "react";
+import supabase from "@/utils/supabaseClient";
 
 export default function LoginPage() {
-  const [email, setEmail] = useState("");
+  const [usernameOrEmail, setUsernameOrEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
+  function validateUsernameOrEmail(input) {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // ตรวจสอบว่าเป็น Email
+    const usernamePattern = /^[a-zA-Z0-9_]+$/; // ตรวจสอบว่าเป็น Username
+
+    if (emailPattern.test(input)) {
+      return "email"; // Input เป็น Email
+    } else if (usernamePattern.test(input)) {
+      return "username"; // Input เป็น Username
+    } else {
+      return "invalid"; // Input ไม่ผ่านการตรวจสอบ
+    }
+  }
+
   async function handleLogin() {
     setError(""); // รีเซ็ตข้อผิดพลาดก่อนหน้า
+    let email = ""; // เก็บค่า email
+
+    //validating user input for usernameOrEmail
+    const userInputType = validateUsernameOrEmail(usernameOrEmail);
+
+    if (userInputType === "invalid") {
+      setError("Username or Email invalid");
+      return;
+    }
+
+    if (userInputType === "username") {
+      const username = usernameOrEmail;
+      //get Email
+      const { data, error } = await supabase
+        .from("users")
+        .select("email")
+        .eq("username", username);
+
+      if (error) {
+        return res.status(500).json({ error: error.message });
+      }
+      email = data[0]["email"];
+    } else {
+      email = usernameOrEmail;
+    }
 
     const response = await fetch("/api/auth/login", {
       method: "POST",
@@ -33,10 +72,10 @@ export default function LoginPage() {
       <h1>Login</h1>
       {error && <p style={{ color: "red" }}>{error}</p>}
       <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
+        type="text" // เปลี่ยนจาก email เป็น text เพื่อรองรับ username
+        placeholder="Username or Email"
+        value={usernameOrEmail}
+        onChange={(e) => setUsernameOrEmail(e.target.value)}
       />
       <input
         type="password"
