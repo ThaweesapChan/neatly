2cd477ea6c07a893139f0cc7c6c7b27c27c0d815
thanapakorn <thanapakorn001@gmail.com>
2024-11-29 09:42:02 +0700

    feat: add search card

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import Homepage from "./homepage";
 import Navbar from "@/component/navbar";
 import Footer from "@/component/footer";
-import Searchresult from "../component/searchresult";
-import Roomcard from "@/component/roomcard";
+import Searchresult from "./searchresult";
 export default function Home() {
   return (
     <div>
       {/* landing page */}
-      <Homepage />
+      <Navbar />
+      <Searchresult />
+      <Footer />
     </div>
   );
 }
