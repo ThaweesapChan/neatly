26677165f52b248192e76ab602b7128bdb08a88a
Arisa Teandum <basarisa.1302@gmail.com>
2024-12-01 22:39:47 +0700

    feat: style ui login page for desktop

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -82,72 +82,80 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="flex flex-col">
+    <div className="flex w-full flex-col">
       <Navbar />
-      <img
-        src="/asset/login3chair_mobile.jpeg"
-        className="h-fit w-full"
-        alt="Login Illustration"
-      />
+      {/* รูปภาพ */}
+      <div className="flex flex-col md:flex-row md:items-start md:gap-20 md:bg-[#F7F7FB]">
+        <img
+          src="/asset/login3chair_mobile.jpeg"
+          className="h-fit w-full md:hidden" // รูปภาพสำหรับ Mobile
+          alt="Login Illustration for Mobile"
+        />
+        <img
+          src="/asset/login3chair.jpeg"
+          className="md:full hidden md:block md:h-screen md:w-1/2" // รูปภาพสำหรับ Desktop
+          alt="Login Illustration for Desktop"
+        />
 
-      {/* กลุ่มเนื้อหาหลังรูปภาพ */}
-      <div className="flex flex-col gap-14 p-10">
-        <h1 className="h-14 w-80 font-notoSerif text-5xl font-medium text-green-800">
-          Log In
-        </h1>
-        <div className="flex flex-col gap-10">
-          <div>
-            <label
-              htmlFor="usernameOrEmail"
-              className="mb-1 block font-medium font-inter text-gray-900"
+        {/* กลุ่มเนื้อหาหลังรูปภาพ */}
+        <div className="md: md:gap-10 md:ml-16 flex flex-col gap-14 p-10 md:mt-48 md:w-[452px] md:p-0">
+          <h1 className="h-14 w-80 font-notoSerif text-5xl font-medium text-green-800 md:text-7xl">
+            Log In
+          </h1>
+          <div className="flex flex-col gap-10 md:mt-10">
+            <div>
+              <label
+                htmlFor="usernameOrEmail"
+                className="mb-1 block font-inter font-medium text-gray-900"
+              >
+                Username or Email
+              </label>
+              <input
+                className="w-full rounded border border-gray-300 p-4 font-inter text-gray-400"
+                id="usernameOrEmail"
+                type="text"
+                placeholder="Enter your username or email"
+                value={usernameOrEmail}
+                onChange={(e) => setUsernameOrEmail(e.target.value)}
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="mb-1 block font-inter font-medium text-gray-900"
+              >
+                Password
+              </label>
+              <input
+                className="w-full rounded border border-gray-300 p-4 text-gray-400"
+                id="password"
+                type="password"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            <button
+              className="rounded bg-orange-600 p-4 font-openSans text-base font-semibold text-white"
+              onClick={handleLogin}
+              disabled={loading} // ปิดการใช้งานปุ่มเมื่อกำลังโหลด
             >
-              Username or Email
-            </label>
-            <input
-              className="w-full rounded border border-gray-300 p-4 text-gray-400"
-              id="usernameOrEmail"
-              type="text"
-              placeholder="Enter your username or email"
-              value={usernameOrEmail}
-              onChange={(e) => setUsernameOrEmail(e.target.value)}
-            />
+              {loading ? "Logging in..." : "Log In"}
+            </button>
+            {/* ข้อความพร้อมลิงก์ไปยังหน้าลงทะเบียน */}
+            <p className="-mt-4 font-inter text-base font-normal text-gray-700">
+              Don't have an account yet?{" "}
+              <Link
+                href="/register"
+                className="font-openSans text-base font-semibold text-orange-500 hover:underline"
+              >
+                Register
+              </Link>
+            </p>
+
+            {error && <p style={{ color: "red" }}>{error}</p>}
           </div>
-          <div>
-            <label
-              htmlFor="password"
-              className="mb-1 block font-medium font-inter text-gray-900"
-            >
-              Password
-            </label>
-            <input
-              className="w-full rounded border border-gray-300 p-4 text-gray-400"
-              id="password"
-              type="password"
-              placeholder="Enter your password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-
-          <button
-            className="rounded bg-orange-600 p-4 font-openSans text-base font-semibold text-white"
-            onClick={handleLogin}
-            disabled={loading} // ปิดการใช้งานปุ่มเมื่อกำลังโหลด
-          >
-            {loading ? "Logging in..." : "Log In"}
-          </button>
-          {/* ข้อความพร้อมลิงก์ไปยังหน้าลงทะเบียน */}
-          <p className="-mt-4 font-inter text-base font-normal text-gray-700">
-            Don't have an account yet?{" "}
-            <Link
-              href="/register"
-              className="font-openSans text-base font-semibold text-orange-500 hover:underline"
-            >
-              Register
-            </Link>
-          </p>
-
-          {error && <p style={{ color: "red" }}>{error}</p>}
         </div>
       </div>
     </div>
