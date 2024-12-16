520627d38fc6ff0e8a2cdbb891736abdc394d156
thanapakorn <thanapakorn001@gmail.com>
2024-12-16 15:35:20 +0700

    feat:add step1-2 and booking detail ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,14 @@
-
+import Homepage from "./homepage";
+import Bookingdetail from "@/component/payment/bookingdetail";
+import Basicinformation from "@/component/payment/step1/idex";
+import Standardrequest from "@/component/payment/step2/idex";
 import RoomProperty from "./agent/roomproperty";
 export default function Home() {
   return (
     <div>
+      <Basicinformation />
+      <Standardrequest />
+      <Bookingdetail />
       <RoomProperty />
     </div>
   );
