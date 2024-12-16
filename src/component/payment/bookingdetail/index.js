94d5b0d286c33bb1ebfdfad4d227841ed755e1d1
thanapakorn <thanapakorn001@gmail.com>
2024-12-16 18:50:20 +0700

    feat: step1-2 and booking detail ui

--- src/component/payment/bookingdetail/index.js
+++ src/component/payment/bookingdetail/index.js
@@ -2,30 +2,30 @@ import React from "react";
 
 export default function Bookingdetail() {
   return (
-      <div>
-        <div className="space-y-4 rounded-lg bg-[#2F4C43] p-4 text-white">
-          <div className="bg- flex items-center justify-between">
-            <h2 className="flex items-center gap-2">
-              <svg
-                className="h-5 w-5"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"
-                />
-              </svg>
-              Booking Detail
-            </h2>
-            <span className="rounded bg-red-100 px-2 py-0.5 text-sm text-red-600">
-              04:55
-            </span>
-          </div>
+    <div>
+      <div className="space-y-4 rounded-lg bg-[#2F4C43] p-4 text-white">
+        <div className="bg- flex items-center justify-between">
+          <h2 className="flex items-center gap-2">
+            <svg
+              className="h-5 w-5"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"
+              />
+            </svg>
+            Booking Detail
+          </h2>
+          <span className="rounded bg-red-100 px-2 py-0.5 text-sm text-red-600">
+            04:55
+          </span>
+        </div>
 
           <div className="grid grid-cols-2 gap-2 text-sm">
             <div>
@@ -58,5 +58,6 @@ export default function Bookingdetail() {
           </div>
         </div>
       </div>
+    </div>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,16 @@ import Homepage from "./homepage";
 import Bookingdetail from "@/component/payment/bookingdetail";
 import Basicinformation from "@/pages/payment/step1/idex";
 import Standardrequest from "@/pages/payment/step2/idex";
+import Basicinformation from "@/pages/payment/step1/idex";
+import Standardrequest from "@/pages/payment/step2/idex";
 import RoomProperty from "./agent/roomproperty";
+import Booking from "./payment";
+import Step3 from "@/pages/payment/step3";
 export default function Home() {
+  console.log(Step3);
   return (
     <div>
-      <Homepage />
+      <Step3 />
     </div>
   );
 }
