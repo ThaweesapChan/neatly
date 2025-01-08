14c9c9a438df23493f05efd39c966d22fa5f7e38
thanapakorn <thanapakorn001@gmail.com>
2025-01-08 11:51:20 +0700

    fix: resolve conflict

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,4 @@
 import Homepage from "./homepage";
-import Searchresultpage from "./searchresultpage";
-import Booking from "./payment";
-import Bookingdetail from "@/component/payment/bookingdetail";
 export default function Home() {
   return (
     <div>
